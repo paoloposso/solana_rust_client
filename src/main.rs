5a45831705a8a28f531833bd7e1d36fb6ced51a8
paolo <pvictorsys@gmail.com>
2022-03-17 20:03:22 -0300

    add transaction confirmations

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,15 @@
-use std::{str::FromStr, error::Error};
+use std::{error::Error, str::FromStr};
 
 use solana_client::rpc_client::RpcClient;
 use solana_program::{pubkey::{Pubkey}};
-use solana_sdk::{commitment_config::CommitmentConfig, system_transaction, signature::{Keypair, Signature}, signer::Signer};
+use solana_sdk::{system_transaction, signature::{Keypair, Signature}, signer::Signer};
+
+const URL: &str = "https://api.devnet.solana.com";
 
 fn main() {
-    // let rpc = RpcClient::new_with_commitment("https://api.devnet.solana.com", CommitmentConfig::confirmed());
-    let rpc = RpcClient::new("https://api.devnet.solana.com");
+    let rpc = RpcClient::new(URL);
+
+    _ = print_basic_operations(&rpc);
 
     let sender = Keypair::new();
     let receiver = Keypair::new();
@@ -45,9 +48,23 @@ fn check_balance(rpc_client: &RpcClient, public_key: &Pubkey) -> core::result::R
 }
 
 fn request_air_drop(rpc_client: &RpcClient, pub_key: &Pubkey, amount_lamports: u64) -> core::result::Result<Signature, Box<dyn Error>> {
-    Ok(rpc_client.request_airdrop(&pub_key, amount_lamports)?)
+    let sig = rpc_client.request_airdrop(&pub_key, amount_lamports)?;
+    loop {
+        let confirmed = rpc_client.confirm_transaction(&sig)?;
+        if confirmed {
+            break;
+        }
+    }
+    Ok(sig)
 }
 
 fn transfer_funds(rpc_client: &RpcClient, sender_keypair: &Keypair, recipient_pub_key: &Pubkey, amount_lamports: u64) -> core::result::Result<Signature, Box<dyn Error>> {
-    Ok(rpc_client.send_transaction(&system_transaction::transfer(&sender_keypair, &recipient_pub_key, amount_lamports, rpc_client.get_latest_blockhash()?))?)
+    let sig = rpc_client.send_and_confirm_transaction(&system_transaction::transfer(&sender_keypair, &recipient_pub_key, amount_lamports, rpc_client.get_latest_blockhash()?))?;
+    Ok(sig)
+}
+
+fn print_basic_operations(rpc_client: &RpcClient) -> Result<(), Box<dyn Error>> {
+    let vec = rpc_client.get_program_accounts(&Pubkey::from_str("HAKGVjYFMfhsaTHMk215ZeVbfuYn1fJgwNsE9iJ24zZ9")?)?;
+    println!("{:?}", vec);
+    Ok(())
 }
