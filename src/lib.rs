e3b69fc0fb16036183f6f677fbb29aedb7cc897c
paolo <pvictorsys@gmail.com>
2022-03-29 20:14:17 -0300

    Fix amount rounding

---
+++ src/lib.rs
@@ -0,0 +1,35 @@
+use std::error::Error;
+use solana_client::rpc_client::RpcClient;
+use solana_program::pubkey::{Pubkey};
+use solana_sdk::{system_transaction, signature::{Keypair, Signature}};
+
+const LAMPORTS_PER_SOL: f64 = 1000000000.0;
+
+pub fn create_keypair() -> Keypair {
+    Keypair::new()
+}
+
+pub fn check_balance(rpc_client: &RpcClient, public_key: &Pubkey) -> Result<f64, Box<dyn Error>> {
+    Ok(rpc_client.get_balance(&public_key)? as f64 / LAMPORTS_PER_SOL)
+}
+
+pub fn request_air_drop(rpc_client: &RpcClient, pub_key: &Pubkey, amount_sol: f64) -> Result<Signature, Box<dyn Error>> {
+    let sig = rpc_client.request_airdrop(&pub_key, (amount_sol * LAMPORTS_PER_SOL) as u64)?;
+    loop {
+        let confirmed = rpc_client.confirm_transaction(&sig)?;
+        if confirmed {
+            break;
+        }
+    }
+    Ok(sig)
+}
+
+pub fn transfer_funds(rpc_client: &RpcClient, sender_keypair: &Keypair, receiver_pub_key: &Pubkey, amount_sol: f64) -> core::result::Result<Signature, Box<dyn Error>> {
+    let amount_lamports = (amount_sol * LAMPORTS_PER_SOL) as u64;
+    
+    Ok(rpc_client.send_and_confirm_transaction(
+        &system_transaction::transfer(
+            &sender_keypair, &receiver_pub_key, 
+            amount_lamports, 
+            rpc_client.get_latest_blockhash()?))?)
+}
--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,28 @@
-use std::{error::Error, str::FromStr};
-
 use solana_client::rpc_client::RpcClient;
-use solana_program::{pubkey::{Pubkey}};
-use solana_sdk::{system_transaction, signature::{Keypair, Signature}, signer::Signer};
+use solana_rust_client::{check_balance, request_air_drop, transfer_funds, create_keypair};
+use solana_sdk::signer::Signer;
 
 const URL: &str = "https://api.devnet.solana.com";
 
 fn main() {
     let rpc = RpcClient::new(URL);
 
-    _ = print_basic_operations(&rpc);
+    let sender = create_keypair();
+    let receiver = create_keypair();
 
-    let sender = Keypair::new();
-    let receiver = Keypair::new();
     println!("Sender: {:?}", sender.pubkey());
     println!("Receiver: {:?}", receiver.pubkey());
 
-    let amount = 100000000;
+    let amount = 0.5;
 
-    if let Ok(airdrop_signature) = request_air_drop(&rpc, &sender.pubkey(), 1000000000) {
+    if let Ok(airdrop_signature) = request_air_drop(&rpc, &sender.pubkey(), 1.0) {
         println!("Airdrop finished! Signature: {:?}",  airdrop_signature);
 
         if let Ok(balance) = check_balance(&rpc, &sender.pubkey()) {
             println!("Sender balance: {:?}", balance);
         }
 
-        let res = transfer_funds(&rpc, &sender, &receiver.pubkey(), amount);
-        match res {
+        match transfer_funds(&rpc, &sender, &receiver.pubkey(), amount) {
             Ok(sig) => { 
                 println!("Transfer of {:?} finished. Signature: {:?}", amount, sig);
                 if let Ok(balance) = check_balance(&rpc, &sender.pubkey()) {
@@ -41,30 +37,4 @@ fn main() {
     } else {
         println!("Airdrop failed");
     }
-}
-
-fn check_balance(rpc_client: &RpcClient, public_key: &Pubkey) -> core::result::Result<u64, Box<dyn Error>> {
-    Ok(rpc_client.get_balance(&public_key)?)
-}
-
-fn request_air_drop(rpc_client: &RpcClient, pub_key: &Pubkey, amount_lamports: u64) -> core::result::Result<Signature, Box<dyn Error>> {
-    let sig = rpc_client.request_airdrop(&pub_key, amount_lamports)?;
-    loop {
-        let confirmed = rpc_client.confirm_transaction(&sig)?;
-        if confirmed {
-            break;
-        }
-    }
-    Ok(sig)
-}
-
-fn transfer_funds(rpc_client: &RpcClient, sender_keypair: &Keypair, recipient_pub_key: &Pubkey, amount_lamports: u64) -> core::result::Result<Signature, Box<dyn Error>> {
-    let sig = rpc_client.send_and_confirm_transaction(&system_transaction::transfer(&sender_keypair, &recipient_pub_key, amount_lamports, rpc_client.get_latest_blockhash()?))?;
-    Ok(sig)
-}
-
-fn print_basic_operations(rpc_client: &RpcClient) -> Result<(), Box<dyn Error>> {
-    let vec = rpc_client.get_program_accounts(&Pubkey::from_str("HAKGVjYFMfhsaTHMk215ZeVbfuYn1fJgwNsE9iJ24zZ9")?)?;
-    println!("{:?}", vec);
-    Ok(())
 }
